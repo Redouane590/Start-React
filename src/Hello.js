1989fda391832098f7b6303568e085c2cabe5c19
Redouane Elmahi <redouaneelmahi@gmail.com>
2023-02-01 16:54:15 +0100

    ajouter un point

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Hello({age, name}) {
-  return <h2>Bonjour, Je m'appelle { name }, j'ai {age} ans, ceci est mon premier composant exporté en default, ici je fais de la transmission de props</h2>
+  return <h2>Bonjour, Je m'appelle { name }, j'ai {age} ans, ceci est mon premier composant exporté en default, ici je fais de la transmission de props.</h2>
 }
 
 export function Hello2(props) {
