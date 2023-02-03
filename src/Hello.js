e33b9179f2ecd75b88dee2feae9c895197fd644e
Redouane Elmahi <redouaneelmahi@gmail.com>
2023-02-03 14:51:34 +0100

    J\'ajoute un peu de style

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 export default function Hello({age, name}) {
-  return <h2>Bonjour, Je m'appelle { name }, j'ai {age} ans, ceci est mon premier composant exporté en default, ici je fais de la transmission de props.</h2>
+
+  return <h2 style={{backgroundColor: 'cyan', color: 'black', borderRadius: '12px', padding: '25px', margin: '25px'}}>Bonjour, Je m'appelle { name }, j'ai {age} ans, ceci est mon premier composant exporté en default, ici je fais de la transmission de props.</h2>
 }
 
 export function Hello2(props) {
