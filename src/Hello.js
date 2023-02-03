1d236456ab09bdba99c99050d7f99aa160dd9317
Redouane Elmahi <redouaneelmahi@gmail.com>
2023-02-03 15:17:59 +0100

    add style with module css

--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,11 @@
 import React from "react";
+import styles from "./Hello.module.css"
+
 
 export default function Hello({age, name}) {
-
-  return <h2 style={{backgroundColor: 'cyan', color: 'black', borderRadius: '12px', padding: '25px', margin: '25px'}}>Bonjour, Je m'appelle { name }, j'ai {age} ans, ceci est mon premier composant exporté en default, ici je fais de la transmission de props.</h2>
+  const color = 'black'
+  return <h2 className={styles.title}>Bonjour, Je m'appelle { name }, j'ai {age} ans, ceci est mon premier composant exporté en default, ici je fais de la transmission de props.
+  j'ai ajouté un peu de style en jsx</h2>
 }
 
 export function Hello2(props) {
